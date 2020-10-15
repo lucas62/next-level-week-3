af40d4500041d6b66f874858445b07a96f635f4b
Lucas Anselmo <lucas.pires100.la@gmail.com>
2020-10-15 12:02:19 -0300

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import path from 'path';
-import 'express-async-errors';
+import cors from 'cors';
 
+import 'express-async-errors';
 
 import './database/connection';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
